466a3c8459da6604036deee43af44d44c363cee0
gurpreet562 <kgurpreet562@gmail.com>
2017-10-15 20:02:55 +0530

    Create view.component.ts

---
+++ view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+				
+				@Component({
+				  selector: 'view-root',
+				  template: `<h1>Viewing Data</h1>`
+				               
+				  
+				})
+				export class ViewComponent {
+				
+				}
